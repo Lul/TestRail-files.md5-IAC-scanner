60b8e001b25d5ea2463fce763c6fa83815df80be
John <59487177+Lul@users.noreply.github.com>
2022-02-24 15:06:14 -0500

    Add files via upload

---
+++ vulnCheck.py
@@ -0,0 +1,21 @@
+import requests
+import re
+
+keywords = ['username', 'password', 'PRIVATE KEY']
+server = input('Server in http:// or https:// format: ')
+
+r = requests.get(f'{server}/files.md5')
+files = re.split(r'\n|  ', r.text)
+
+for index, value in enumerate(files):
+    if index % 2 != 0:
+        r = requests.get(f'{server}/{value}')
+        if 'No direct script access allowed' not in r.text and 'Directory Listing Denied' not in r.text:
+            if r.status_code != 500:
+                for key in keywords:
+                    if key in r.text:
+                        print(f'{server}/{value} - Status Code: {r.status_code} - INTERESTING KEYWORD: {key}')
+                    else:
+                        print(f'{server}/{value} - Status Code: {r.status_code}')
+                        break
+
