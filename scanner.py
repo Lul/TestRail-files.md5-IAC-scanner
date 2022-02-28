5dad66ecb59737112cdef5d37c61f196a6fbb89a
John <59487177+Lul@users.noreply.github.com>
2022-02-28 09:52:34 -0500

    Update and rename vulnCheck.py to scanner.py

--- scanner.py
+++ scanner.py
@@ -2,6 +2,8 @@ import requests
 import re
 
 keywords = ['username', 'password', 'PRIVATE KEY']
+matches = []
+
 server = input('Server in http:// or https:// format: ')
 if server[-1] != '/':
     server+='/'
@@ -16,7 +18,9 @@ for index, value in enumerate(files):
             if r.status_code != 500:
                 for key in keywords:
                     if key in r.text:
-                        print(f'{server}{value} - Status Code: {r.status_code} - INTERESTING KEYWORD: {key}')
-                    else:
-                        print(f'{server}{value} - Status Code: {r.status_code}')
-                        break
+                        matches.append(key)
+                if len(matches) > 0:
+                    print(f'{server}{value} - Status Code: {r.status_code} - INTERESTING KEYWORD: {matches}')
+                    matches.clear()
+                else:
+                    print(f'{server}{value} - Status Code: {r.status_code}')
